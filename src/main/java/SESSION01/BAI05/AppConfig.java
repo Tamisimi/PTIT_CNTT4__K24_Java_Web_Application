60d7d8e7869a83be0ac1591cf4096f4ea399fb37
Tamisimi <alexmd0107@gmail.com>
2026-04-05 23:15:04 +0700

    Create AppConfig.java

---
+++ src/main/java/SESSION01/BAI05/AppConfig.java
@@ -0,0 +1,9 @@
+package SESSION01.BAI05;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example")
+public class AppConfig {
+}
