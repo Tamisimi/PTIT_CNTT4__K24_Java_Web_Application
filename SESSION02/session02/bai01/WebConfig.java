dc0f7a7e934344f9d33874987e17b5ba050c1c30
Tamisimi <alexmd0107@gmail.com>
2026-04-06 22:08:19 +0700

    Create WebConfig.java

---
+++ SESSION02/session02/bai01/WebConfig.java
@@ -0,0 +1,21 @@
+package session02.bai01;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.demo.controller")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
