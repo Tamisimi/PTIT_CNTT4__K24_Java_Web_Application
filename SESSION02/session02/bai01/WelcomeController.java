22965bff316c1286105305f2f82012c9b221e589
Tamisimi <alexmd0107@gmail.com>
2026-04-06 22:09:10 +0700

    Create WelcomeController.java

---
+++ SESSION02/session02/bai01/WelcomeController.java
@@ -0,0 +1,16 @@
+package session02.bai01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String showWelcome(Model model) {
+        model.addAttribute("staffName", "Nguyễn Văn An");
+        model.addAttribute("department", "Phòng Kỹ thuật");
+        return "welcome";
+    }
+}
